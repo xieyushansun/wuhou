dbb3556a8ead2c08cd8d87c17dce38fb2571e406
Lyan <liyan2015@std.uestc.edu.cn>
2020-10-03 10:29:56 +0800

    新增soultable的相关资源及应用

--- src/main/resources/static/webapp/js/lay-config.js
+++ src/main/resources/static/webapp/js/lay-config.js
@@ -1,3 +1,7 @@
+/*
+ * @Author: liyan
+ * @LastEditors: liyan
+ */
 /**
  * date:2019/08/16
  * author:Mr.Chung
@@ -18,13 +22,6 @@ layui.config({
     miniMenu: "layuimini/miniMenu", // layuimini菜单扩展
     miniTab: "layuimini/miniTab", // layuimini tab扩展
     miniTheme: "layuimini/miniTheme", // layuimini 主题扩展
-    miniTongji: "layuimini/miniTongji", // layuimini 统计扩展
-    step: 'step-lay/step', // 分步表单扩展
-    treetable: 'treetable-lay/treetable', //table树形扩展
-    tableSelect: 'tableSelect/tableSelect', // table选择扩展
-    iconPickerFa: 'iconPicker/iconPickerFa', // fa图标选择扩展
-    echarts: 'echarts/echarts', // echarts图表扩展
-    echartsTheme: 'echarts/echartsTheme', // echarts图表主题扩展
     wangEditor: 'wangEditor/wangEditor', // wangEditor富文本扩展
-    layarea: 'layarea/layarea', //  省市县区三级联动下拉选择器
+    soulTable: 'soulTable/soulTable.slim', // 可以拖拽表格 
 });
