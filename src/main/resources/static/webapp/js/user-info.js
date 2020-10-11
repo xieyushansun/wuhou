0c84eb37d59c27158455015e892524e43c6445a6
Lyan <liyan2015@std.uestc.edu.cn>
2020-10-11 20:08:30 +0800

    增加用户信息页面

---
+++ src/main/resources/static/webapp/js/user-info.js
@@ -0,0 +1,36 @@
+layui.use(['form', 'table'], function() {
+    var user = getUser();
+    var form = layui.form,
+        table = layui.table;
+    
+    form.val('user-info', {
+        username: user.username,
+        nickname: user.nickname,
+        rolename: user.rolename
+    });
+
+    var permissionNames = (function() {
+        var arr = [];
+        layui.each(user.permissions, function(index1, item1) {
+            layui.each(PERMISSIONS, function(index2, item2) {
+                if (item1 === item2.permission) {
+                    arr.push({permission_name: item2.permission_name});
+                    return false;
+                }
+            });
+        });
+        return arr;
+    })();
+
+    // permissionNames = [{permission_name: 1},{permission_name: 1},{permission_name: 1},{permission_name: 1}]
+
+    table.render({
+        elem: '#permission-table',
+        id: 'permission-table',
+        data: permissionNames,
+        cols: [[
+            {field: 'permission_name', title: '权限', align: 'center'}
+        ]],
+        width: 300
+    });
+});
