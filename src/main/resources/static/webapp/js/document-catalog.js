4c8f03cd01e17db789dd763f072f4daa5bfec090
xieyushan <cuitxieyushan@163.com>
2020-10-12 22:22:18 +0800

    前端bug再修改

--- src/main/resources/static/webapp/js/document-catalog.js
+++ src/main/resources/static/webapp/js/document-catalog.js
@@ -31,8 +31,7 @@ layui.use(['table', 'form', 'layer'], function () {
             {field: 'recordTime', title: '著录时间', width: 100, minWidth: 80},
             {title: '存放路径', width: 100, templet: function(d) {
                 return d.diskPath + ':\\' + d.storePath;
-            }},
-            {title: '操作', minWidth: 150, toolbar: '#currentTableBar', align: "center", width: 150, fixed: 'right'}
+            }}
         ]],
         request: {
             pageName: 'currentPage',
@@ -66,8 +65,9 @@ layui.use(['table', 'form', 'layer'], function () {
             page: {
                 curr: 1
             },
+
             done: function(res, curr, count) {
-                if (res.code === 0) {
+                if (res.code === 0 && count > 0) {
                     $(".start-btn").removeClass('layui-disabled');
                     $('.down-btn').addClass('layui-hide').removeAttr('href');
                 } else if (res.code === 12) {
